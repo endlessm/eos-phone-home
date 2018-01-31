ce1985a50f647b439a621db1df39ab29007b0f0b
Will Thompson <will@willthompson.co.uk>
2018-01-31 14:03:54 +0000

    Add just enough build system for Jenkins to run tests
    I appreciate that this is a hack, and in the time it's taken me to write
    this, I could probably have written a setup.py or real Autotools/Meson
    build.

---
+++ autogen.sh
@@ -0,0 +1,4 @@
+#!/bin/true
+# This stub script is here to convince Jenkins to run our tests. The exported
+# tarball does not include this file because .gitattributes instructs
+# git-archive to exclude it.
