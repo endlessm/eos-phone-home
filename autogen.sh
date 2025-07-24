2365b3fdb61ae040684b5900e397121423dedd42
Jian-Hong Pan <jhp@endlessos.org>
2025-07-24 13:46:03 +0800

    Install files with meson
    EOS 7 will be based on GNOME OS, which uses BuildStream as the build
    tool. So, install the files with meson, instead of deb tool. The meson
    works in both EOS 6 and 7's build environment.
    
    Fixes: https://github.com/endlessm/eos-build-meta/issues/40

--- autogen.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/true
-# This stub script is here to convince Jenkins to run our tests. The exported
-# tarball does not include this file because .gitattributes instructs
-# git-archive to exclude it.
